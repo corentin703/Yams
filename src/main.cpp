a3dc97e6ce535928060870cb5e797cbce8f23dc3
Corentin VÉROT <verot.corentin@protonmail.com>
2020-05-15 12:42:26 +0200

    Regénération de la doc Amélioration de la qualité du logo Mise en place du logo dans À propos

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,21 @@
  *
  * @section intro Introduction
  *
- *  Cette application est une adaptation du jeu de société Yams à la reconnaissance d'image.
- *  Elle est réalisée avec le framework [Qt](https://www.qt.io) pour la partie graphique et la bibliothèque [OpenCV](https://opencv.org) pour la reconaissance d'image.
+ *  Cette application est une adaptation du jeu de société Yams à la reconnaissance d'image. <br/>
+ *  Elle est réalisée avec le framework [Qt](https://www.qt.io) pour la partie graphique et la bibliothèque [OpenCV](https://opencv.org) pour la reconaissance d'image. <br/>
+ *
+ *  Le jeu prend la forme d'une fenêtre présentant les scores des différents joueurs sous la forme d'un tableau sur la droite,
+ *  le rendu caméra sur la gauche et les meilleurs choix en bas de la fenêtre. <br/>
+ *  Si l'utilisateur ne trouve pas les choix proposés judicieux,
+ *  il peut néanmoins choisir l'action qu'il souhaite faire en cliquant sur le bouton correspondant à l'action
+ *  qu'il désire réaliser dans la colonne gauche du tableau des scores. Les boutons des actions déjà réalisées seront grisés. <br/>
+ *
+ *  Un rappel des règles du jeu se trouve dans le menuAide - Règles du jeu. <br/>
+ *  Le Yams étant un jeux se jouant avec un nombre minimal de deux joueurs sans maximum, il
+ *  est possible de jouer avec l’application avec autant de contraintes. <br/>
+ *  La reconnaissance d’image servant à détecter les dés peut ne pas être infaillible.
+ *  C’est pourquoi un bouton \a Redétecter a été disposé à droite des boutons représentant les meilleurs choix.
+ *
  *
  *  Pour un fonctionnement optimal de la reconnaissance d'image, merci de :
  *
